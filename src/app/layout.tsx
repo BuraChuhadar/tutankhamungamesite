a52913ba2e17ac5078fcf7b8fb664f93ac947d30
Bura Chuhadar <burachuhadar@outlook.com>
2025-06-13 22:20:44 -0400

    Increasing the main container width.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
               />
             </div>
             <div className="px-40 flex flex-1 justify-center py-5">
-              <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
+              <div className="layout-content-container flex flex-col max-w-[1000px] flex-1">
                 {children}
               </div>
             </div>
