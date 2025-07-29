62677a6bb2e5eb03de4270d62259529dc0418b84
Bura Chuhadar <burachuhadar@outlook.com>
2025-07-28 22:27:34 -0400

    Changing the default image for discord.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = "tutankhamun-game";
 export const CMS_NAME = "Markdown";
 export const SITE_URL = process.env.SITE_URL || "https://tutankhamungame.com";
-export const HOME_OG_IMAGE_URL = `${SITE_URL}/assets/images/main-title.png`;
+export const HOME_OG_IMAGE_URL = `${SITE_URL}/assets/blog/images/gpuioptimzer.png`;
